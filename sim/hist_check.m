fae997889ea1c9db04befe8c4c97049e7b7bb93c
pedro <hdlguy@hotmail.com>
2020-07-16 09:02:40 -0400

    improved matlab plot script

--- sim/hist_check.m
+++ sim/hist_check.m
@@ -1,14 +1,34 @@
 clear;
-Nbins = 100;
-centers = 8*(-Nbins/2:Nbins/2-1)/Nbins;
+
 
 d1 = load("-ascii","gng_data_out.txt");
-d1 = d1/(2^11);
+Nsamp = length(d1);
+Nbins = 40;
+span  = 8;
+centers = span*(-Nbins/2:Nbins/2-1)/Nbins;
+
+d1 = d1/(2^11); % 5.11 fixed point format.
 
 [dd,xx] = hist(d1, centers);
+dd = dd/Nsamp;
 
-[gg,xx] = hist(randn(1000000,1), centers);
+[gg,xx] = hist(randn(Nsamp,1), centers);
+gg = gg/Nsamp;
 
 stem(xx,dd, 'r*'); 
 hold on;
 stem(xx,gg, 'b');
+
+% compute theoretical distribution.
+function retval = cdf (x, mu, sigma)
+  retval = (1/2)*(1+erf((x-mu)/(sigma*sqrt(2))));
+endfunction
+mu=0; 
+sigma=1.0; 
+x=centers; 
+f = cdf(centers+(span/Nbins)/2, mu, sigma) - cdf(centers-(span/Nbins)/2, mu, sigma);
+
+plot(xx,f,'m');
+
+
+
